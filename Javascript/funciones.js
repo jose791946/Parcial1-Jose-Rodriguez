dba2f3c638b13a9b0eeb46422335d96121d7e85e
jose791946 <jose791946@gmail.com>
2022-09-30 21:08:12 -0300

    Agregue lo realizado

---
+++ Javascript/funciones.js
@@ -0,0 +1,8 @@
+function mostrar(){
+ let clic = document.getElementById("nuevaVianda");
+ if (clic.selected){
+    document.getElementById("formularios").innerHTML;
+ }else{
+    document.getElementById("formularios").disable;
+ }
+}
